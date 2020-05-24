69e260749e0c4ee2b37f33174e90f855fa82d6b4
Mason Smith <masonium@gmail.com>
2020-05-24 01:11:27 -0700

    art-util: refract_dir implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod models;
 mod poisson;
 mod spatial_hash;
+mod math;
 
 pub use models::add_box;
 pub use poisson::PoissonSampling;
 pub use spatial_hash::SpatialHash2D;
+pub use math::refract_dir;
---
+++ src/math.rs
@@ -0,0 +1,25 @@
+use nalgebra as na;
+
+pub fn refract_dir(incident: &na::Vector2<f32>, normal: &na::Vector2<f32>, n1: f32, n2: f32) -> Option<na::Vector2<f32>> {
+
+    let cos_ti = normal.dot(incident) / (normal.norm() * incident.norm());
+    let n = n1 / n2;
+    let sin_tr = n * (1.0 - cos_ti * cos_ti).sqrt();
+    // sufficiently close to the critical angle.
+    if (sin_tr - 1.0).abs() < 1e-5 {
+        return None;
+    }
+
+    // refract or reflect, depending on the angle
+    let new_dir = if sin_tr > 1.0 {
+        // total internal reflection
+        incident - 2.0 * incident.dot(&normal) * normal
+    } else {
+        // refract at the new angle.
+        let c1 = cos_ti;
+        let c2 = (1.0 - n * n * (1.0 - c1 * c1)).sqrt();
+        n * incident + (n * c1 - c2) * normal
+    };
+
+    Some(new_dir)
+}
