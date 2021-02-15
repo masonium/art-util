a8af1c89ceb73f7f649dcf331cfb4af853f2ab76
Mason Smith <masonium@gmail.com>
2021-02-15 02:50:26 -0800

    Rect: add functions, remove TryFrom trait dependency

--- src/math/types.rs
+++ src/math/types.rs
@@ -1,22 +1,49 @@
 use nalgebra as na;
-use na::{Point2, Vector2};
+use na::{Point2, Vector2, center};
 use std::fmt::Debug;
-use std::convert::TryFrom;
 
-pub trait Scalar: na::RealField + Into<f64> + TryFrom<f64> + Debug + Copy + 'static{}
-impl<T> Scalar for T where T: na::RealField + Into<f64> + TryFrom<f64> + Debug + Copy + 'static {}
+pub trait Scalar: na::RealField + Into<f64> /*+ TryFrom<f64>*/ + Debug + Copy + 'static{}
+impl<T> Scalar for T where T: na::RealField + Into<f64> /*+ TryFrom<f64>*/ + Debug + Copy + 'static {}
 
-pub struct Rect<F: na::RealField> {
+/// Simple 2D rectangle, for common bounding-box style operations.
+#[derive(Clone, Copy, Debug)]
+pub struct Rect<F: Scalar> {
     pub p: [Point2<F>; 2]
 }
 
-impl<F: na::RealField> Rect<F> {
+impl<F: Scalar> Rect<F> {
+    /// Create a rectangle from the extreme corner points.
     pub fn from_points(a: &Point2<F>, b: &Point2<F>) -> Rect<F> {
 	Rect { p: [*a, *b] }
     }
 
+    /// Create a rectangle from the minimum point and rectangle
+    /// dimension.
     pub fn from_point_dim(a: &Point2<F>, d: &Vector2<F>) -> Rect<F> {
 	Rect { p: [*a, *a + *d] }
+    }
+
+    /// Return the (width, height) of the rectangle as a 2d vector.
+    pub fn dim(&self) -> Vector2<F> {
+	self.p[1] - self.p[0]
+    }
+
+    /// Return the corners of the rect in (-x, -y), (x, -y) (-x, y),
+    /// (x, y) order.
+    pub fn corners(&self) -> [Point2<F>; 4] {
+	[self.p[0], Point2::new(self.p[1].x, self.p[0].y),
+	 self.p[1], Point2::new(self.p[0].x, self.p[1].y)]
+    }
+
+    pub fn midpoints(&self) -> [Point2<F>; 4] {
+	let c = self.corners();
+	[center(&c[0], &c[1]),
+	 center(&c[1], &c[2]),
+	 center(&c[2], &c[3]),
+	 center(&c[3], &c[0])]
+    }
+    pub fn center(&self) -> Point2<F> {
+	center(&self.p[0], &self.p[1])
     }
 
 }
